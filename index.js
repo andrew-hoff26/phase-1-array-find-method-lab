57e3d8a0c4c16afd2f88e7ac31a67635ca6c29fd
andrew <andrew_hoffman262@outlook.com>
2023-10-23 14:35:20 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+  const winObject = record.find(obj => obj.result === "W")
+  console.log(winObject)
+  if (winObject) {
+    return winObject.year;
+  } else {
+    return undefined;
+  }
+}
